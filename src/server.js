1c9df29209ab0908c9ab934d443f60043fb39d4a
yogajulianp <yogajprasutiyo@gmail.com>
2022-08-16 13:34:53 +0700

    add Bookshelf API

---
+++ src/server.js
@@ -0,0 +1,23 @@
+/* eslint-disable linebreak-style */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+// eslint-disable-next-line eol-last
+init();
